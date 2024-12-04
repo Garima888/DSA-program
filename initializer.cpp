12315c10e92866794158799ac8f971d5a572d282
Garima888 <garimapatel951@gmail.com>
2024-12-04 22:35:14 +0530

    Add files via upload

---
+++ initializer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[]={3,6,4,3,5,65,43,24,22,24,66,445,44,77,66,5454,33,333};
+    int n=sizeof(arr);
+    cout<<n<<endl;
+   for(int i=0; i<=18; i++)
+    {
+
+  cout<<arr[i]<<" ";
+    }
+return 0;
+}
