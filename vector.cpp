61b4549aff44ffc151b87884e6f5b3d2eb389579
Garima888 <garimapatel951@gmail.com>
2024-12-09 22:33:58 +0530

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+   // int arr[n];
+   vector<int> arr(n,7);
+ //   for(int i=0; i<n; i++)
+   // {
+     //   cin>>arr[i];
+    //}
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl; 
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    arr.push_back(10);
+    cout<<arr.size()<<endl;
+    cout<<arr.capacity()<<endl;
+    return 0;
+}
